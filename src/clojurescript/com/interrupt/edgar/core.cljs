f3c0f012e4af93efb33d7cfde3998e53dfbb4d9b
Timothy Washington <twashing@interruptsoftware.com>
2018-07-28 14:28:55 -0700

    Visualizing streams SMA, EMA, Bollinger Bands

--- src/clojurescript/com/interrupt/edgar/core.cljs
+++ src/clojurescript/com/interrupt/edgar/core.cljs
@@ -12,12 +12,23 @@
 
   (let [chart-options (clj->js {:rangeSelector {"selected" 1}
                                 :title {:text "TSLA Stock Price"}
-                                :series [{:name "TSLA"
+                                :series [{:name "Tick List"
                                           :data []
                                           :tooltip {"valueDecimals" 2}}
-                                         {:name "TSLA 2"
+                                         {:name "SMA"
                                           :data []
-                                          :tooltip {"valueDecimals" 2}}]})]
+                                          :tooltip {"valueDecimals" 2}}
+                                         {:name "EMA"
+                                          :data []
+                                          :tooltip {"valueDecimals" 2}}
+                                         {:name "Bollinger Band"
+                                          :type "arearange"
+                                          :lineWidth 0
+                                          :fillOpacity 0.3
+                                          :zIndex 0
+                                          :data []
+                                          :tooltip {"valueDecimals" 2}
+                                          :color (aget (.-colors (.getOptions js/Highcharts)) 0)}]})]
 
     (.log js/console "chart-options: " chart-options)
     (.stockChart js/Highcharts "container" chart-options)))
@@ -27,21 +38,31 @@
   #_(.log js/console (.-data e))
   #_(.log js/console (js/eval (clj->js (read-transit (.-data e)))))
 
-  (let [a (aget (.-charts js/Highcharts) 0)
-        b (aget (.-series a) 0)
+  (let [charts (aget (.-charts js/Highcharts) 0)
+        tick-series (aget (.-series charts) 0)
+        sma-series (aget (.-series charts) 1)
+        ema-series (aget (.-series charts) 2)
+        upper-series (aget (.-series charts) 3)
+        lower-series (aget (.-series charts) 4)
 
         {{:keys [last-trade-time last-trade-price]} :tick-list
          {:keys [last-trade-price-average]} :sma-list
-         {:keys [last-trade-price-exponential]} :ema-list :as message}
+         {:keys [last-trade-price-exponential]} :ema-list
+         {:keys [upper-band lower-band]} :bollinger-band :as message}
 
         (read-transit (.-data e))
 
-        tick [last-trade-time last-trade-price]]
+        tick [last-trade-time last-trade-price]
+        sma [last-trade-time last-trade-price-average]
+        ema [last-trade-time last-trade-price-exponential]
+        bband [last-trade-time lower-band upper-band]]
 
-    (.log js/console message)
-    (.addPoint b (js/eval (clj->js tick)) true false)
-
-    ))
+    ;; (.log js/console d)
+    (.log js/console (js/eval (clj->js message)))
+    (.addPoint tick-series (js/eval (clj->js tick)) true false)
+    (.addPoint sma-series (js/eval (clj->js sma)) true false)
+    (.addPoint ema-series (js/eval (clj->js ema)) true false)
+    (.addPoint upper-series (js/eval (clj->js bband)) true false)))
 
 (defn doc-ready-handler []
   (let[ready-state (. js/document -readyState)]
